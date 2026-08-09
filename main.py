a61e273308395e17884d024a3fc45062266a4387
advikjaiswal <149669664+advikjaiswal@users.noreply.github.com>
2026-08-09 09:40:27 +0530

    Clean up main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# main.py
+from stringos.demo import main
 
-from agentos.recursive_agent import Agent
 
 if __name__ == "__main__":
-    prompt = input("🤖 Enter task for Root Agent: ")
-    root_agent = Agent(name="RootAgent", task=prompt)
-    root_agent.run()
+    main()
